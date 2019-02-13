945b09e0241b094c877e4f7e277c20c972587ba8
sushmamanne <42704977+sushmamanne@users.noreply.github.com>
2019-02-13 10:48:24 -0600

    ..

---
+++ Python/Lab1/source/2.py
@@ -0,0 +1,9 @@
+# created Dictionary
+dictionary =  {
+            "John" : [("Physics", 80), ("Science", 95)],
+            "Daniel" : [ ("History", 75), ("Science", 90)],
+            "Mark" : [ ("Maths", 100), ("Social", 95)]
+       }
+# printing items in the dictionary
+for x, y in dictionary.items():
+  print(x, y)
---
+++ Python/Lab1/source/3.py
@@ -0,0 +1,23 @@
+# declared list 1
+py = input("Enter the list of students who are in python class")
+py_list = py.split(",")
+# declared list 2
+web = input("Enter list of students who are in web class")
+w_list = web.split(",")
+# created list for common students
+common =[]
+# created list for uncommon students
+uncommon = []
+for y in py_list:
+    if y in w_list:
+        common.append(y)
+    else:
+        uncommon.append(y)
+for x in w_list:
+    if x in py_list:
+        pass
+    else:
+        uncommon.append(x)
+# printing results
+print("common students:", common)
+print("uncommon students:",uncommon)
