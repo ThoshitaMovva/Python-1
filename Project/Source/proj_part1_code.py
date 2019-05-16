5d644cf3569d534ddf5cab95eb49edbcfeaa1ac2
sindhusha-t <42704977+sushmamanne@users.noreply.github.com>
2019-05-16 02:02:08 -0500

    ..

---
+++ Project/Source/proj_part1_code.py
@@ -0,0 +1,182 @@
+﻿%%time
+import pandas as pd
+import numpy as np
+import json
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from scipy.sparse import hstack
+
+import matplotlib.pyplot as plt
+np.random.seed(0)
+
+df = pd.read_json('News_Category_Dataset_v2.json', lines=True)
+
+df['hds']=df['headline']+df['short_description']
+
+import nltk
+import string
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+stemmer = PorterStemmer()
+
+def text_process(mess):
+    nopunc=[char for char in mess if char not in string.punctuation]
+    new1=''.join(nopunc)
+    new2=[stemmer.stem(word) for word in new1]
+    new3=''.join(new2)
+    return[word for word in new3.split()if word.lower()not in stopwords.words('english') ]
+
+df['hds'].head()
+
+df['hds'].head(5).apply(text_process)
+
+title_tr, title_te, category_tr, category_te = train_test_split(df['hds'],df['category'])
+title_tr, title_de, category_tr, category_de = train_test_split(title_tr,category_tr)
+print("Training: ",len(title_tr))
+print("Developement: ",len(title_de),)
+print("Testing: ",len(title_te))
+
+from wordcloud import WordCloud
+text = " ".join(title_tr)
+wordcloud = WordCloud().generate(text)
+plt.figure()
+plt.subplots(figsize=(20,12))
+wordcloud = WordCloud(
+    background_color="white",
+    max_words=len(text),
+    max_font_size=40,
+    relative_scaling=.5).generate(text)
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
+
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import nltk
+nltk.download('popular')
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import VarianceThreshold
+from imblearn.over_sampling import SMOTE
+from sklearn.dummy import DummyClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+#from sklearn.metrics import accuracy_score
+#from sklearn.model_selection import cross_val_score, KFold
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+import seaborn as sns
+
+
+# Data Preprocessing
+# Vectorizing the data using Bag of words (BOW)
+tokenizer = nltk.tokenize.RegexpTokenizer(r"\w+")
+stop_words = nltk.corpus.stopwords.words("english")
+vectorizer = CountVectorizer(tokenizer=tokenizer.tokenize, stop_words=stop_words)
+
+vectorizer.fit(iter(title_tr))
+Xtr = vectorizer.transform(iter(title_tr))
+Xde = vectorizer.transform(iter(title_de))
+Xte = vectorizer.transform(iter(title_te))
+
+encoder = LabelEncoder()
+encoder.fit(category_tr)
+Ytr = encoder.transform(category_tr)
+Yde = encoder.transform(category_de)
+Yte = encoder.transform(category_te)
+
+#Feature Reduction
+
+print("Number of features before reduction : ", Xtr.shape[1])
+selection = VarianceThreshold(threshold=0.001)
+Xtr_whole = copy.deepcopy(Xtr)
+Ytr_whole = copy.deepcopy(Ytr)
+selection.fit(Xtr)
+Xtr = selection.transform(Xtr)
+Xde = selection.transform(Xde)
+Xte = selection.transform(Xte)
+print("Number of features after reduction : ", Xtr.shape[1])
+
+#Sampling the data
+
+labels = list(set(Ytr))
+counts = []
+for label in labels:
+    counts.append(np.count_nonzero(Ytr == label))
+plt.pie(counts, labels=labels, autopct='%1.1f%%')
+plt.show()
+
+sm = SMOTE(random_state=42)
+Xtr, Ytr = sm.fit_sample(Xtr, Ytr)
+labels = list(set(Ytr))
+counts = []
+for label in labels:
+    counts.append(np.count_nonzero(Ytr == label))
+plt.pie(counts, labels=labels, autopct='%1.1f%%')
+plt.show()
+
+
+#Decision Tree Model
+
+#Decision tree
+dt = DecisionTreeClassifier()
+dt.fit(Xtr, Ytr)
+pred = dt.predict(Xde)
+print(classification_report(Yde, pred, target_names=encoder.classes_))
+
+# Random Forest Model
+
+rf = RandomForestClassifier(n_estimators=40)
+rf.fit(Xtr, Ytr)
+pred = rf.predict(Xde)
+print(classification_report(Yde, pred, target_names=encoder.classes_))
+
+#Multinomial Naive Bayes Model
+
+nb = MultinomialNB()
+nb.fit(Xtr, Ytr)
+pred = nb.predict(Xde)
+print(classification_report(Yde, pred, target_names=encoder.classes_))
+
+
+#Support Vector Classification
+from sklearn.svm import SVC
+svc = SVC()
+svc.fit(Xtr, Ytr)
+pred = svc.predict(Xde)
+print(classification_report(Yde, pred, target_names=encoder.classes_))
+
+#Predicting using Naive Bayes 
+print("\n\nPredicting test data using Multinomial Naive Bayesian")
+pred_final = nb.predict(Xte)
+print(classification_report(Yte, pred_final, target_names=encoder.classes_))
+
+reverse_vocabulary = {}
+vocabulary = vectorizer.vocabulary_
+for word in vocabulary:
+    index = vocabulary[word]
+    reverse_vocabulary[index] = word
+
+vector = vectorizer.transform(iter(['Nasa scientists are good']))
+indexes = vector.indices
+for i in indexes:
+    print (reverse_vocabulary[i])
+
+nb1 = MultinomialNB()
+nb1.fit(Xtr_whole, Ytr_whole)
+coefs = nb1.coef_
+target_names = encoder.classes_
+
+for i in range(len(target_names)):
+    words = []
+    for j in coefs[i].argsort()[-20:]:
+        words.append(reverse_vocabulary[j])
+    print (target_names[i], '-', words, "\n")
+
+
